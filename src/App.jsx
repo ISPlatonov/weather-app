14d0dcaadbd52e77733c8d483396969be0e8603f
ISPlatonov <isplatonov.official@gmail.com>
2024-07-06 15:54:08 +0200

    implemented add. func.

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,35 @@ import React, { useState } from "react";
 import { fetchWeather } from "./api/fetchWeather";
 
 const App = () => {
-  const [weatherData, setWeatherData] = useState(null);
   const [cityName, setCityName] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
   const [error, setError] = useState(null);
+  const [tempUnit, setTempUnit] = useState(localStorage.getItem('tempUnit') || 'C');
+  const [recentSearches, setRecentSearches] = useState([]);
+
+  const toggleTempUnit = () => {
+    setTempUnit((prevUnit) => (prevUnit === 'C' ? 'F' : 'C'));
+    // save the tempUnit to localStorage
+    localStorage.setItem('tempUnit', tempUnit === 'C' ? 'F' : 'C');
+  };
+
+  const fetchAndDisplayWeather = async (city = cityName) => {
+    try {
+      const data = await fetchWeather(city, tempUnit); // Assuming fetchWeather can take tempUnit as an argument
+      setWeatherData(data);
+      setCityName("");
+      setError(null);
+
+      const updatedSearches = [city, ...recentSearches.filter(c => c !== city)].slice(0, 5);
+      setRecentSearches(updatedSearches);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
 
   const fetchData = async (e) => {
     if (e.key === "Enter") {
-      try {
-        const data = await fetchWeather(cityName);
-        setWeatherData(data);
-        setCityName("");
-        setError(null);
-      } catch (error) {
-        setError(error.message);
-      }
+      await fetchAndDisplayWeather();
     }
   };
 
@@ -32,12 +47,10 @@ const App = () => {
       {weatherData && (
         <div>
           <h2>
-            {weatherData.location.name}, {weatherData.location.region},{" "}
-            {weatherData.location.country}
+            {weatherData.location.name}, {weatherData.location.region}, {weatherData.location.country}
           </h2>
           <p>
-            Temperature: {weatherData.current.temp_c} °C (
-            {weatherData.current.temp_f} °F )
+            Temperature: {tempUnit === 'C' ? `${weatherData.current.temp_c} °C` : `${weatherData.current.temp_f} °F`}
           </p>
           <p>Condition: {weatherData.current.condition.text}</p>
           <img
@@ -49,6 +62,19 @@ const App = () => {
           <p>Visibility: {weatherData.current.vis_km} km</p>
         </div>
       )}
+      <div>
+        <h3>Recent Searches:</h3>
+        <ul>
+          {recentSearches.map((city, index) => (
+            <li key={index} onClick={() => { setCityName(city); fetchAndDisplayWeather(city); }}>
+              {city}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <button onClick={toggleTempUnit}>
+        Switch to {tempUnit === 'C' ? 'Fahrenheit' : 'Celsius'}
+      </button>
     </div>
   );
 };
